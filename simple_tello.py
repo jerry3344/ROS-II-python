a52bd400d2b6333c74b131918e4ccc62de526b7c
jerry3344 <wujiearegood@hotmail.com>
2022-11-16 15:41:06 +0800

    feat: 增加simple_tello 指令

---
+++ simple_tello.py
@@ -0,0 +1,9 @@
+import rospy
+import time
+import sys
+from geomatry_msgs.msg import Twist
+from std_msgs.msg import Empty,UInt8, Float64MultiArray
+from time import sleep
+from tello_driver.msg import TelloStatus
+
+
